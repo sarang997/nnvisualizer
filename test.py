ad35a0fd81dd2b5fa3632d17ceee962ae0f05281
sarangKnimbus <sarang.bhatnagar@knimbus.com>
2023-01-04 10:05:03 +0530

    accessing the group of layers

--- test.py
+++ test.py
@@ -3,9 +3,11 @@ from nnvisualizer.Setup import Setup
 from nnvisualizer.Utils import Utils
 
 #Setting up the model before visualizing
-path = '/home/knimbus/Downloads/archive/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+path = '/home/sarang/Downloads/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
 setup = Setup(model_path =path)
 model = setup.loadModel() 
 info = setup.modelInfo()
-print(info['layers'])
-print(info['totalLayers'])
+print(info)
+f1 = model['block1_conv1']
+f2 = f1.items()
+print(f2)
