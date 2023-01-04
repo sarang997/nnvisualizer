b986aa7d408518ba2cb39295512b1ce2bfbaa414
sarang997 <bhatnagar.sarang1@gmail.com>
2023-01-04 10:08:02 +0530

    testing username for git

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from nnvisualizer.Setup import Setup
 from nnvisualizer.Utils import Utils
 
 #Setting up the model before visualizing
+#
 path = '/home/sarang/Downloads/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
 setup = Setup(model_path =path)
 model = setup.loadModel() 
