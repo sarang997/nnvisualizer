8c762b287d328f9aceae66a77e5e152aef74665a
sarang997 <bhatnagar.sarang1@gmail.com>
2023-01-07 12:34:06 +0530

    showing pot changes in test.py

--- test.py
+++ test.py
@@ -1,14 +1,11 @@
 
 from nnvisualizer.Setup import Setup
 from nnvisualizer.Utils import Utils
+import numpy as np
 
 #Setting up the model before visualizing
 #
 path = '/home/sarang/Downloads/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
 setup = Setup(model_path =path)
-model = setup.loadModel() 
-info = setup.modelInfo()
-print(info)
-f1 = model['block1_conv1']
-f2 = f1.items()
-print(f2)
+setup.showPlot()
+print(weights)
